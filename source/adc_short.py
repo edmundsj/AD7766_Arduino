617399763b1ef1947c68dd7262cfa32d17495afd
edmundsj <jordan.e@berkeley.edu>
2020-08-14 11:56:53 -0700

    refactored repository, moving source code to src folder

--- source/adc_short.py
+++ source/adc_short.py
@@ -15,6 +15,7 @@ from adc_communication import *
 maxBytes = 90000
 samplingFrequency = 125 # 125kHz is the actual measured sampling rate, correct within 1Hz.
 samplingPeriod = 1 / samplingFrequency
+averaged = False
 
 startSystemTime = time.perf_counter()
 dataBuffer = getADCBytes(maxBytes=maxBytes) # Gets data from our ADC
@@ -40,27 +41,32 @@ samplingTimes = samplingPeriod * np.arange(0, numberSamples, 1)
 frequencies = samplingFrequency / numberSamples * np.arange(0, numberSamples, 1)
 
 zeroMeanData = adcValues - np.mean(adcValues)
-fftData = 10*np.log10(np.square(np.abs(np.fft.fft(zeroMeanData))))
-fftData -= np.max(fftData)
+fftData = 10*np.log10(np.square(np.abs(np.fft.fft(zeroMeanData*2/numberSamples))))
+fftDataRMS = fftData - 3 # subtract 3dB, or a factor of 2 in power
 signalAmplitudeVpp = 0.05
 signalFrequency = 1
-fftData += 20*np.log10(signalAmplitudeVpp)
+hanningCorrectionFactor = 1.633
 
-hanningWindow = scipy.signal.windows.hann(numberSamples)
+hanningWindow = hanningCorrectionFactor * scipy.signal.windows.hann(numberSamples)
 hanningFilteredData = zeroMeanData * hanningWindow
-hanningFFTData = np.square(np.abs(np.fft.fft(hanningFilteredData)))
-hanningFFTData = 10*np.log10(hanningFFTData)
-hanningFFTData -= np.max(hanningFFTData)
-hanningFFTData += 20*np.log10(signalAmplitudeVpp)
+hanningFFTDataPower = np.square(np.abs(np.fft.fft(hanningFilteredData*2/numberSamples)))
+hanningFFTDataPowerRMS = hanningFFTDataPower / 2
+convolutionKernel = np.ones(500)/500.0
+hanningFFTDataPowerAverage = np.convolve(hanningFFTDataPowerRMS, convolutionKernel, mode='same')
+hanningFFTDataPowerAveragedBV = 10*np.log10(hanningFFTDataPowerAverage)
+hanningFFTDataPowerdBV = 10*np.log10(hanningFFTDataPowerRMS)
 
 np.savetxt('data/arduino_raw_data.csv', dataBuffer, delimiter=',')
 np.savetxt('data/adc_values.csv', adcValues, delimiter=',')
 
 samplingWindow = samplingPeriod * numberSamples
-#theoreticalPSD = np.square(np.sinc((frequencies-signalFrequency)* samplingWindow))
-theoreticalPSD = np.square(np.abs(np.fft.fft(hanningWindow)))
+#theoreticalPSD = np.square(signalAmplitudeVpp/2*np.sinc((frequencies-signalFrequency)* samplingWindow)) / 2
+theoreticalData = hanningWindow * signalAmplitudeVpp / 2 * np.cos(2*np.pi * signalFrequency * samplingTimes)
+theoreticalPSD = np.square(np.abs(np.fft.fft(theoreticalData*2/numberSamples))) / 2 # The 2 converts from amplitude to RMS
+if(averaged):
+    theoreticalPSD = np.convolve(theoreticalPSD, convolutionKernel, mode='same')
+
 theoreticalPSD = 10*np.log10(theoreticalPSD)
-theoreticalPSD += 20*np.log10(signalAmplitudeVpp)
 
 fig, axes = plt.subplots(1, 2)
 axes[0].plot(samplingTimes, 1000*zeroMeanData)
@@ -68,10 +74,16 @@ axes[0].set_xlabel('time (ms)')
 axes[0].set_ylabel('voltage (mV)')
 axes[0].set_xlim(0, 5)
 
-axes[1].plot(frequencies[1:], hanningFFTData[1:], 'b')
+if(averaged):
+    axes[1].plot(frequencies[1:], hanningFFTDataPowerAveragedBV[1:], 'b')
+else:
+    axes[1].plot(frequencies[1:], hanningFFTDataPowerdBV[1:], 'b')
+    #axes[1].plot(frequencies[1:], fftDataRMS[1:], 'b')
 axes[1].set_xlabel('frequency (kHz)')
-axes[1].set_ylabel('amplitude/power (dBV)')
+axes[1].set_ylabel('dBV rms')
 axes[1].set_ylim(-180, 0)
-axes[1].set_xlim(0, 64)
-axes[1].plot(frequencies[1:], theoreticalPSD[1:], 'g')
+axes[1].set_xlim(0.0, 62.5)
+axes[1].plot(frequencies[1:], theoreticalPSD[1:], 'g', linestyle='dashed')
+
+#print(noiseAverage)
 plt.show()
